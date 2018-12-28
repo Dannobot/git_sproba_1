b26a410bb4a675dcb7a69c54e42f2bd6865e09ba
Danulo Rekhnuke <danqwe1996@gmail.com>
2018-12-28 19:05:22 +0200

    vjenorm

--- handler.py
+++ handler.py
@@ -1,40 +1,80 @@
+from __future__ import print_function
+import boto3
+import botocore.exceptions
+import hmac
+import hashlib
+import base64
 import json
 import uuid
-import boto3
-import os
+import time
 
 
 
-
-
-
-
-
-
+USER_POOL_ID = 'eu-central-1_CakwLVb47'
+CLIENT_ID = '1mll39npejv4maltcii43u8mv3'
 
 
 def hello(event, context):
-    body = {
-        "message": "v: 1.1"
-    }
 
-    response = {
-        "body": json.dumps(body)
-    }
+    return  [
+        {
+            "name":"A",
+            "age":20,
+            "cars":["qwe","ewq"]
+        },
 
-    return response
+        {
+            "name":"C",
+            "age":22,
+            "cars":["qwe","ewq"]
+        },
+
+        {
+            "name":"B",
+            "age":21,
+            "cars":["qwe","ewq"]
+        },
+        ]
 
 
+def psadmin(event, context):
+    client = boto3.client('cognito-idp')
+    UserPool_Id = USER_POOL_ID
+    List_Group_Name = list()
+    dicti = {}
+    response = client.list_users(
+        UserPoolId=UserPool_Id
+    )
+    List_Group_Users = list()
+    dictionnaire={}
+    for user in response["Users"]:
+        dictionnaire["UserName"]=user["Username"]
+
+        response2 = client.admin_list_groups_for_user(
+            Username=user["Username"],
+            UserPoolId=UserPool_Id
+        )
+        List_Group_Name=list()
+        for group in  response2["Groups"]:
+            List_Group_Name.append(group["GroupName"])
+
+        dictionnaire["Groups"]=List_Group_Name
+        List_Group_Users.append(dictionnaire)
+        dictionnaire={}
+
+    return(List_Group_Users)
 
 
+def groupadd(event, context):
+    UserPool_Id = USER_POOL_ID
+    Client_Id = CLIENT_ID
 
-def imageR(event, context):
-    body = {
-        "message": "V: image"
-    }
+    client = boto3.client('cognito-idp')
 
-    response = {
-        "body": json.dumps(body)
-    }
-
-    return response
+    body = event
+    username = body['username']
+    adding = client.admin_add_user_to_group(
+        UserPoolId=UserPool_Id,
+        Username=username,
+        GroupName='Users')
+    return username
